7b3177812314817e8e65a11ec20e9b9f70d89d7d
Erik Zscheile <zseri.devel@ytrizja.de>
2020-06-05 01:44:27 +0200

    PacketStream: inline poll_next async block

--- lib/src/packet_stream.rs
+++ lib/src/packet_stream.rs
@@ -1,10 +1,11 @@
-use crate::helpers::poll_future;
 use bytes::{Buf, BufMut, Bytes, BytesMut};
-use futures_util::{io::AsyncWrite, pin_mut, sink::Sink, stream::Stream};
+use futures_util::io::{AsyncRead, AsyncWrite};
+use futures_util::{pin_mut, ready, sink::Sink, stream::Stream};
 use smol::Async;
 use std::net::TcpStream;
 use std::pin::Pin;
 use std::task::{Context, Poll};
+use tracing::debug;
 
 pub struct PacketStream {
     stream: Async<TcpStream>,
@@ -29,37 +30,39 @@ impl Stream for PacketStream {
 
     fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
         let this = Pin::into_inner(self);
-        poll_future(cx, async move {
-            loop {
-                if this.buf_in.len() >= 2 && this.in_xpdlen.is_none() {
-                    this.in_xpdlen = Some(this.buf_in.get_u16().into());
-                }
-                if let Some(expect_len) = this.in_xpdlen {
-                    if this.buf_in.len() >= expect_len {
-                        // we are done, if we reach this,
-                        // the length spec was already removed from buf_xin
-                        this.in_xpdlen = None;
-                        return Ok(Some(this.buf_in.split_to(expect_len).freeze()));
-                    }
-                }
-                // we need more data
-                let mut rdbuf = [0u8; 8192];
-                // the `read` might yield, and it should not leave any part of
-                // `this` in an invalid state
-                // assumption: `read` only yields if it has not read (and dropped) anything yet.
-                use futures_util::io::AsyncReadExt;
-                let len = this.stream.read(&mut rdbuf).await?;
-                tracing::debug!("received {} bytes", len);
-                if len == 0 {
-                    return Ok(None);
-                }
-                this.buf_in.extend_from_slice(&rdbuf[..len]);
+
+        loop {
+            if this.buf_in.len() >= 2 && this.in_xpdlen.is_none() {
+                this.in_xpdlen = Some(this.buf_in.get_u16().into());
             }
-        })
-        .map(|ret| match ret {
-            Ok(o) => o.map(Ok),
-            Err(e) => Some(Err(e)),
-        })
+            if let Some(expect_len) = this.in_xpdlen {
+                if this.buf_in.len() >= expect_len {
+                    // we are done, if we reach this,
+                    // the length spec was already removed from buf_xin
+                    this.in_xpdlen = None;
+                    return Poll::Ready(Some(Ok(this.buf_in.split_to(expect_len).freeze())));
+                }
+            }
+
+            // we need more data
+            // the `read` might yield, and it should not leave any part of
+            // `this` in an invalid state
+            // assumption: `read` only yields if it has not read (and dropped) anything yet.
+            let mut rdbuf = [0u8; crate::MAX_U16LEN];
+            let stream = &mut this.stream;
+            pin_mut!(stream);
+            match ready!(stream.poll_read(cx, &mut rdbuf)) {
+                Err(e) => return Poll::Ready(Some(Err(e))),
+                Ok(0) => {
+                    debug!("received EOF");
+                    return Poll::Ready(None);
+                }
+                Ok(len) => {
+                    debug!("received {} bytes", len);
+                    this.buf_in.extend_from_slice(&rdbuf[..len]);
+                }
+            }
+        }
     }
 }
 
@@ -97,7 +100,7 @@ impl<B: AsRef<[u8]>> Sink<B> for PacketStream {
             // data two times, and thus invalidating the data stream
             // assumption: `write` only yields if it has not written anything yet
             let len = pollerfwd!(stream.as_mut().poll_write(cx, &buf_out[..]));
-            tracing::debug!("sent {} bytes", len);
+            debug!("sent {} bytes", len);
             // drop written part
             buf_out.advance(len);
         }
